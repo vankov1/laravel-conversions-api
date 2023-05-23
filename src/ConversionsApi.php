3bc8ab7f4c6f3124f2669ae2cf3dfca96e86f135
Val Vankov <val@vankov.me>
2023-05-23 10:11:13 +0300

    Update ConversionsApi.php

--- src/ConversionsApi.php
+++ src/ConversionsApi.php
@@ -83,7 +83,7 @@ class ConversionsApi
     public function sendEvents(): PromiseInterface
     {
         $pixelId = $this->pixel_id ? $this->pixel_id : config('conversions-api.pixel_id');
-        $eventRequest = (new EventRequestAsync($pixelId)
+        $eventRequest = (new EventRequestAsync($pixelId))
             ->setEvents($this->events);
 
         $testCode = $this->test_code ? $this->test_code : config('conversions-api.test_code');
